dcfabc3f512169d20e64e4133430fa62784cf9d5
MaanLad <maanlad25@gmail.com>
2024-04-15 14:18:30 +0530

    bearere removed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken"
 export const verifyToken=async(req,res,next)=>{
     try{
          let token=req.header("Authorization")
-        // let token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWNiODNhZDY0YzVmMzk4NmNlYzMzMyIsImlhdCI6MTcxMzE1ODIzOH0.3jopqqsKYGZfu4U6R_aFAxOIx_TJz51-dBq9kJxY_W4"
          if(!token){
             return res.status(403).send("Access denied")
          }
