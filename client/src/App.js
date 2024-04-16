270ddc0222b0e4a3176117eb44f717b4e8153d29
MaanLad <maanlad25@gmail.com>
2024-04-16 23:27:30 +0530

    navbar edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import React, { useMemo } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from 'scenes/homePage'
 import LoginPage from 'scenes/loginPage'
 import ProfilePage from 'scenes/profilePage'
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme"
+import Navbar from 'scenes/navbar'
 
 const App = () => {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LoginPage/>}/>
-      <Route path='/home' element={<HomePage/>}/>
-      <Route path='/profile/:userId' element={< ProfilePage/>}/>
-    </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/profile/:userId' element={< ProfilePage />} />
+          <Route path='/nav' element={< Navbar />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,8 +1,82 @@
+import {useState} from "react";
+import { 
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    MenuItem,
+    Select,
+    useTheme,
+    FormControl,
+    useMediaQuery
+ } from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+}from '@mui/icons-material'
+import { setMode,setLogout } from "state";
+import { useDispatch,useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "component/FlexBetween";
+
+
+
 const Navbar=()=>{
+    const {isMobileMenuToggled,setIsMobileMenuToggled} =useState(false);
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const user=useSelector((state)=>state.user)
+    const isNonMobileScreens=useMediaQuery("(min-width:1000px)")
+
+    const theme=useTheme();
+    const neutralLight=theme.palette.neutral.light;
+    const dark=theme.palette.neutral.dark;
+    const background=theme.palette.background.default;
+    const primaryLight=theme.palette.primary.light;
+    const alt=theme.palette.background.alt;
+
+    // const fullName=`${user.firstName} ${user.lastName}`;
+
+
     return(
-        <div>
-            Hello this is Navbar
-        </div>
+        <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+            <FlexBetween gap={"1.75rem"}>
+                <Typography
+                fontWeight="bold"
+                fontSize="clamp(1rem ,2rem ,2.25rem)"
+                color="primary"
+                onClick={()=>navigate("/home")}
+                sx={{
+                    "&:hover":{
+                        color:primaryLight,
+                        cursor:"pointer"
+                    }
+                }}
+                >
+                    Sociopedia
+                </Typography>
+                {isNonMobileScreens && (
+                    <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+                        <InputBase placeholder="Search..."/>
+                    </FlexBetween>
+                )}
+            </FlexBetween>
+            {isNonMobileScreens?(<FlexBetween>
+                <IconButton onClick={()=>{dispatch(setMode())}}>
+                    {theme.palette.mode==="dark"?(
+                        <DarkMode sx={{fontSize:"25px"}}/>
+                    ):(
+                        <LightMode sx={{color:dark ,fontSize:"25px"}}/>
+                    )}
+                </IconButton>
+            </FlexBetween>):(<IconButton></IconButton>)}
+        </FlexBetween>
     )
 }
 
