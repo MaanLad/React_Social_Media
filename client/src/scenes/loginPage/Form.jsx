42ec753fc1126e9ec1c54e6fbb559d9c8c1c07f1
MaanLad <maanlad25@gmail.com>
2024-04-23 16:12:12 +0530

    login ui started

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,11 @@
+import {Box} from "@mui/material";
+
+const Form=()=>{
+    return (
+      <Box>
+        Helo
+      </Box>  
+    );
+}
+
+export default Form;
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,46 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form.jsx";
+
 const LoginPage=()=>{
+const theme=useTheme();
+const isNonMobileScreens=useMediaQuery("(min-width:1000px)");
+
     return(
-        <div>
-            Hello this is loginpage
-        </div>
+        <Box>
+            <Box
+            width='100%'
+            backgroundColor={theme.palette.background.alt}
+            p="1rem 6%"
+            textAlign='center'
+            >
+
+                <Typography 
+                fontWeight="bold"
+                fontSize="32px"
+                color='primary'
+                >
+                    Sociopedia
+                </Typography>
+            </Box>
+            <Box
+                width={isNonMobileScreens?"50%":"90%"}
+                justifyContent='center'
+                backgroundColor={theme.palette.background.alt}
+                p='2rem'
+                m='2rem auto'
+                borderRadius='15px'
+                >
+
+                    <Typography 
+                    fontWeight='500'
+                    variant="h5"
+                    sx={{mb:"1.5rem"}}
+                    >
+                    Welcome to Socipedia, the Social Media for Sociopaths!
+                    </Typography>
+                <Form/>
+                </Box>
+        </Box>
     )
 }
 
